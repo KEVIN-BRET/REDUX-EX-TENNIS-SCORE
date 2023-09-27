eb2964c912b8002988396a19de9a1ce736b361e4
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-09-27 12:08:06 +0200

    Correction exercice

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { createStore } from "https://cdn.skypack.dev/redux@4.0.5";
 
-
 // on trouve les éléments dans le document HTML
 const score = document.getElementById("score");
 const player1Button = document.getElementById("player-1");
@@ -9,20 +8,26 @@ const resetButton = document.getElementById("reset");
 const pauseButton = document.getElementById("pause");
 
 player1Button.addEventListener("click", () => {
-	// Ce code s'exécute lorsque le bouton "Point Joueur 1" est cliqué
+  // Ce code s'exécute lorsque le bouton "Point Joueur 1" est cliqué
+  // On envoie une action avec dispatch
+  // pointScored est un action creator (une fonction qui retourne une action)
+  // il faut donc exécuter la fonction pour obtenir l'action et l'envoyer au store
+  store.dispatch(pointScored("player1"));
 });
 
 player2Button.addEventListener("click", () => {
-	// Ce code s'exécute lorsque le bouton "Point Joueur 2" est cliqué
+  // Ce code s'exécute lorsque le bouton "Point Joueur 2" est cliqué
+  store.dispatch(pointScored("player2"));
 });
 
 resetButton.addEventListener("click", () => {
-	// Ce code s'exécute lorsque le bouton "Remettre à zéro" est cliqué
+  // Ce code s'exécute lorsque le bouton "Remettre à zéro" est cliqué
+  store.dispatch(restartGame());
 });
 
 pauseButton.addEventListener("click", function () {
-	// Ce code s'exécute lorsque le bouton "Pause / Reprendre" est cliqué
-	store.dispatch(playPause());
+  // Ce code s'exécute lorsque le bouton "Pause / Reprendre" est cliqué
+  store.dispatch(playPause());
 });
 
 /**
@@ -34,61 +39,141 @@ pauseButton.addEventListener("click", function () {
  * @param {'player1' | 'player2'} advantage
  */
 function updateScoreText(
-	playing,
-	winner = null,
-	player1Score = 0,
-	player2Score = 0,
-	advantage = null
+  playing,
+  winner = null,
+  player1Score = 0,
+  player2Score = 0,
+  advantage = null
 ) {
-	if (winner) {
-		if (winner === "player1") {
-			score.innerHTML = "Joueur 1 gagne";
-		} else {
-			score.innerHTML = "Joueur 2 gagne";
-		}
-	} else if (playing === false) {
-		score.innerHTML = "C'est la pause";
-	} else {
-		let text = "Le score est: " + player1Score + " - " + player2Score;
-		if (advantage) {
-			if (advantage === "player1") {
-				text += " avantage joueur 1";
-			} else {
-				text += " avantage joueur 2";
-			}
-		}
-		score.innerHTML = text;
-	}
+  if (winner) {
+    if (winner === "player1") {
+      score.innerHTML = "Joueur 1 gagne";
+    } else {
+      score.innerHTML = "Joueur 2 gagne";
+    }
+  } else if (playing === false) {
+    score.innerHTML = "C'est la pause";
+  } else {
+    let text = "Le score est: " + player1Score + " - " + player2Score;
+    if (advantage) {
+      if (advantage === "player1") {
+        text += " avantage joueur 1";
+      } else {
+        text += " avantage joueur 2";
+      }
+    }
+    score.innerHTML = text;
+  }
 }
 
-// A vous de jouer !
-
+// Le state
 const initialState = {
-	player1: 0,
-	player2: 0,
-	advantage: null,
-	winner: null,
-	playing: true,
-}
+  // Le score de chacun des joueurs
+  player1: 0,
+  player2: 0,
+  // Si il y a 40-40 quel joueur a l'avantage
+  // On utilise null si pas d'avantage
+  advantage: null,
+  // Qui a gagné ?
+  // Si la partie est en cours on utilise null
+  winner: null,
+  // La partie est-elle en cours ?
+  playing: true
+};
 
-const playPause = () => ({ type: 'playPause' });
+// Les actions creators
 
+// mettre en pause / reprendre le jeu
+const playPause = () => ({ type: "playPause" });
+
+// redémarrer le jeu
+const restartGame = () => ({ type: "restart" });
+
+// un joueur a marqué un point
+// on passe en paramètre le joueur qui a marqué
+const pointScored = (player) => ({
+  type: "pointScored",
+  payload: { player: player }
+});
+
+// le reducer contient la logique
+// c'est une fonction qui reçoit le state et une action
 function reducer(state, action) {
-	if (action.type === 'playPause') {
-		return {
-			...state,
-			playing: !state.playing,
-		}
-	}
-	return state;
+  // si l'action est de type "restart"
+  if (action.type === "restart") {
+    // on retourne le state initial
+    return initialState;
+  }
+  // si l'action est de type "playPause"
+  if (action.type === "playPause") {
+    // on retourne un nouvel objet
+    return {
+      // qui est une copie du state
+      ...state,
+      // mais on replace la propriété playing
+      playing: !state.playing
+    };
+  }
+  // lorsqu'un joueur marque un point
+  if (action.type === "pointScored") {
+    const player = action.payload.player;
+    const otherPlayer = player === "player1" ? "player2" : "player1";
+    if (state.winner) {
+      // le jeu est fini, on ne peut pas marquer
+      // on retourne le state
+      return state;
+    }
+    if (state.playing === false) {
+      // le jeu est en pause, on ne peut pas marquer
+      // on retourne le state
+      return state;
+    }
+    const currentPlayerScore = state[player];
+    if (currentPlayerScore <= 15) {
+      // le joueur qui a marqué est à 0 ou 15 => on ajoute 15
+      return { ...state, [player]: currentPlayerScore + 15 };
+    }
+    if (currentPlayerScore === 30) {
+      // le joueur qui a marqué est à 30 => on passe à 40
+      return { ...state, [player]: 40 };
+    }
+    // si le joueur est déjà à 40
+    if (currentPlayerScore === 40) {
+      // si l'autre joueur n'est pas à 40
+      if (state[otherPlayer] !== 40) {
+        // le joueur a gagné !
+        return { ...state, winner: player };
+      }
+      // si le joueur a l'avantage
+      if (state.advantage === player) {
+        // le joueur a gagné !
+        return { ...state, winner: player };
+      }
+      // si personne n'as l'avantage
+      if (state.advantage === null) {
+        // le joueur a maintenant l'avantage !
+        return { ...state, advantage: player };
+      }
+      // sinon c'est l'autre joueur qui a l'avantage
+      // l'autre joueur perd l'avantage
+      return { ...state, advantage: null };
+    }
+  }
+  return state;
 }
 
+// on crée le store
 const store = createStore(reducer, initialState);
 
+// subscribe permet de suivre les changements de state
 store.subscribe(() => {
-		const state = store.getState();
-		updateScoreText(state.playing);
-	})
-
-;
-
+  // on utilise getState pour récupérer le state
+  const state = store.getState();
+  updateScoreText(
+    state.playing,
+    state.winner,
+    state.player1,
+    state.player2,
+    state.advantage
+  );
+});
